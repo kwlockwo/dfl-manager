83aa19d21604cc470d24941b4db46f1b96031d63
Keith Lockwood <kwlockwo@gmail.com>
2026-06-28 09:31:14 +1000

    Add safl flag to skip AFL fixture loader in run_refresh.sh
    Allows skipping the AflFixtureLoaderHandler step by passing "safl"
    as the second argument, while keeping the round info and job
    generator steps running.

--- bin/run_refresh.sh
+++ bin/run_refresh.sh
@@ -3,22 +3,37 @@ export CLASSPATH=$APP_HOME/target/dflmngr.jar:$APP_HOME/target/dependency/*
 
 TZ=Austrlia/Melbourne
 run=$1
+skip=$2
 
 if [[ $run == "now" ]]; then
     echo "Running Now"
-    java -classpath $CLASSPATH net.dflmngr.handlers.AflFixtureLoaderHandler && \
-    java -classpath $CLASSPATH net.dflmngr.handlers.DflRoundInfoCalculatorHandler && \
-    java -classpath $CLASSPATH net.dflmngr.scheduler.generators.StartRoundJobGenerator &&\
-    java -classpath $CLASSPATH net.dflmngr.scheduler.generators.EndRoundJobGenerator && \
-    java -classpath $CLASSPATH net.dflmngr.scheduler.generators.ResultsJobGenerator
+    if [[ $skip != "safl" ]]; then
+        java -classpath $CLASSPATH net.dflmngr.handlers.AflFixtureLoaderHandler && \
+        java -classpath $CLASSPATH net.dflmngr.handlers.DflRoundInfoCalculatorHandler && \
+        java -classpath $CLASSPATH net.dflmngr.scheduler.generators.StartRoundJobGenerator &&\
+        java -classpath $CLASSPATH net.dflmngr.scheduler.generators.EndRoundJobGenerator && \
+        java -classpath $CLASSPATH net.dflmngr.scheduler.generators.ResultsJobGenerator
+    else
+        java -classpath $CLASSPATH net.dflmngr.handlers.DflRoundInfoCalculatorHandler && \
+        java -classpath $CLASSPATH net.dflmngr.scheduler.generators.StartRoundJobGenerator &&\
+        java -classpath $CLASSPATH net.dflmngr.scheduler.generators.EndRoundJobGenerator && \
+        java -classpath $CLASSPATH net.dflmngr.scheduler.generators.ResultsJobGenerator
+    fi
 else
     if [[ $(date +%u) -eq 1 ]]; then
         echo "Monday Running"
-        java -classpath $CLASSPATH net.dflmngr.handlers.AflFixtureLoaderHandler && \
-        java -classpath $CLASSPATH net.dflmngr.handlers.DflRoundInfoCalculatorHandler && \
-        java -classpath $CLASSPATH net.dflmngr.scheduler.generators.StartRoundJobGenerator && \
-        java -classpath $CLASSPATH net.dflmngr.scheduler.generators.EndRoundJobGenerator && \
-        java -classpath $CLASSPATH net.dflmngr.scheduler.generators.ResultsJobGenerator
+        if [[ $skip != "safl" ]]; then
+            java -classpath $CLASSPATH net.dflmngr.handlers.AflFixtureLoaderHandler && \
+            java -classpath $CLASSPATH net.dflmngr.handlers.DflRoundInfoCalculatorHandler && \
+            java -classpath $CLASSPATH net.dflmngr.scheduler.generators.StartRoundJobGenerator && \
+            java -classpath $CLASSPATH net.dflmngr.scheduler.generators.EndRoundJobGenerator && \
+            java -classpath $CLASSPATH net.dflmngr.scheduler.generators.ResultsJobGenerator
+        else
+            java -classpath $CLASSPATH net.dflmngr.handlers.DflRoundInfoCalculatorHandler && \
+            java -classpath $CLASSPATH net.dflmngr.scheduler.generators.StartRoundJobGenerator && \
+            java -classpath $CLASSPATH net.dflmngr.scheduler.generators.EndRoundJobGenerator && \
+            java -classpath $CLASSPATH net.dflmngr.scheduler.generators.ResultsJobGenerator
+        fi
     else
         echo "Not Monday Skipping"
     fi
